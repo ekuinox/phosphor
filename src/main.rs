65d1fd4d5e0c43b92051a87e027b964a44ddf107
lm9 <depkey@me.com>
2019-05-02 03:59:27 +0900

    はーーそういう

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![feature(proc_macro_hygiene)]
+#![feature(decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+use rocket::Request;
+
+mod routes;
+
+use routes::*;
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index, article_create, acount_signup])
+        .register(catchers![not_found])
+        .launch();
 }
---
+++ src/routes.rs
@@ -0,0 +1,23 @@
+use rocket::Request;
+use rocket_contrib::json::Json;
+
+#[get("/")]
+pub fn index() -> &'static str {
+    "Hello, world!"
+}
+
+// アカウントを生成
+#[post("/account/signup", data = "<account>")]
+pub fn acount_signup(account: String) -> String {
+    format!("account/signup {:?}", account)
+}
+
+#[post("/article/create", data = "<article>")]
+pub fn article_create(article: String) -> String {
+    format!("/article/create {:?}", article)
+}
+
+#[catch(404)]
+pub fn not_found(req: &Request) -> String {
+    format!("404 {}", req)
+}
