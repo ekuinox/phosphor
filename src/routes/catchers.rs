48a46dc6d2d64ef1ec0e5ee3bebefe80274e999b
lm9 <depkey@me.com>
2019-06-13 22:51:24 +0900

    ResponseBaseをcontrollersの方に移動した.implもいくつか追加した

--- src/routes/catchers.rs
+++ src/routes/catchers.rs
@@ -1,6 +1,6 @@
 use rocket::Request;
 use rocket_contrib::json::Json;
-use crate::routes::ResponseBase;
+use crate::controllers::ResponseBase;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Error {
@@ -9,28 +9,22 @@ pub struct Error {
 
 pub type ErrorResponse = ResponseBase<Error>;
 
-impl ErrorResponse {
-    pub fn new(error: Error) -> ErrorResponse {
-        ErrorResponse { success: false, data: error.clone() }
-    }
-}
-
 #[catch(400)]
 pub fn bad_request(request: &Request) -> Json<ErrorResponse> {
-    Json(ErrorResponse::new(Error { message: "Bad request".to_string() }))
+    Json(ErrorResponse::fail(Error { message: "Bad request".to_string() }))
 }
 
 #[catch(404)]
 pub fn not_found(request: &Request) -> Json<ErrorResponse> {
-    Json(ErrorResponse::new(Error { message: "Not found".to_string() }))
+    Json(ErrorResponse::fail(Error { message: "Not found".to_string() }))
 }
 
 #[catch(422)]
 pub fn unprocessable_entity(request: &Request) -> Json<ErrorResponse> {
-    Json(ErrorResponse::new(Error { message: "Unprocessable entity".to_string() }))
+    Json(ErrorResponse::fail(Error { message: "Unprocessable entity".to_string() }))
 }
 
 #[catch(500)]
 pub fn internal_error(request: &Request) -> Json<ErrorResponse> {
-    Json(ErrorResponse::new(Error { message: "Internal error".to_string() }))
+    Json(ErrorResponse::fail(Error { message: "Internal error".to_string() }))
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,3 @@
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ResponseBase<T> {
-    success: bool,
-    data: T
-}
-
 pub mod access_tokens;
 pub mod catchers;
 pub mod users;
