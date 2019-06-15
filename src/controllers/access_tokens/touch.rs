98048c1a53e4b16de4b89d183c80575a18dcc8ea
lm9 <depkey@me.com>
2019-06-15 15:22:08 +0900

    ResponseBase化

--- src/controllers/access_tokens/touch.rs
+++ src/controllers/access_tokens/touch.rs
@@ -1,6 +1,7 @@
 use diesel::sqlite::SqliteConnection;
 use serde::{Deserialize, Serialize};
 use crate::models::{access_token::*, user::*};
+use crate::controllers::ResponseBase;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Request {
@@ -8,14 +9,29 @@ pub struct Request {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Response {
-    success: bool,
-    user: Option<PrivateUserData>
+pub enum Error {
+    InternalError,
+    BadCredentials
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Data {
+    user: PrivateUserData
+}
+
+impl Data {
+    pub fn new(user: PrivateUserData) -> Data {
+        Data {
+            user: user
+        }
+    }
+}
+
+pub type Response = ResponseBase<Data, Error>;
 
 pub fn touch(request: &Request, connection: &SqliteConnection) -> Response {
     match User::auth(&request.token, &connection) {
-        Some(user) => Response { success: true, user: Some(user.to_private()) },
-        None => Response { success: false, user: None }
+        Some(user) => Response::success(Data::new(user.to_private())),
+        None => Response::fail(Error::BadCredentials)
     }
 }
